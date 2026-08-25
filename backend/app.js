84fa9e533f9ee27d059e7e3aceb4110937205148
Yash Poojari <yashpoojari94@gmail.com>
2026-08-25 19:24:10 +0530

    Configure server for Railway

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,6 @@ app.use("/auth", userRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`server is running on port ${PORT}`);
 });
